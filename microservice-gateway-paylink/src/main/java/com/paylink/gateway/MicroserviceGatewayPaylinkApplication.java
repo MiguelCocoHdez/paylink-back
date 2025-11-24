45e8854b74afc9030420b3b620605bbd4e52f635
MIGUEL\migue <miguelcocohernandez5@gmail.com>
2025-11-24 18:24:08 +0100

    feat: Gateway funcional

---
+++ microservice-gateway-paylink/src/main/java/com/paylink/gateway/MicroserviceGatewayPaylinkApplication.java
@@ -0,0 +1,13 @@
+package com.paylink.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceGatewayPaylinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceGatewayPaylinkApplication.class, args);
+	}
+
+}
---
+++ microservice-gateway-paylink/src/test/java/com/paylink/gateway/MicroserviceGatewayPaylinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paylink.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceGatewayPaylinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
